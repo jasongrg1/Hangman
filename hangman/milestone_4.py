0ab65bfbdb46394ff74c4bb5fef39953676f4707
Jason Gurung <jasongrg@Jasons-MacBook-Pro.local>
2023-10-19 05:34:47 +0100

    added milestone_4.py with Hangman class

---
+++ hangman/milestone_4.py
@@ -0,0 +1,50 @@
+import random
+
+word_list = ['banana', 'mango', 'lychee', 'strawberry', 'grapes']
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+
+        self.word = random.choice(word_list)
+        self.word_guessed = []
+        for i in range(len(self.word)):
+            self.word_guessed.append("_")
+        
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = []
+        
+    
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print (f"Good guess! {guess} is in the word.")
+            for i in range(len(self.word)):
+                if guess == self.word[i]:
+                    self.word_guessed[i] = guess
+            self.num_letters -= 1
+            
+        else:
+            print (f"Sorry, {guess} is not in the word. Try again.")
+
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Enter a single letter: ")
+            if not len(guess) == 1 and guess.isalpha():
+                print ("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+
+    
+
+
+
+
+game = Hangman(word_list)
+
+game.ask_for_input()
