0a1651cfb64dc43162bdb25820c1b6fbbf8b7c62
Jason Gurung <jasongrg@Jasons-MacBook-Pro.local>
2023-10-19 04:45:13 +0100

    added milestone_3.py with check_guess and ask_for_input funcitons.

---
+++ hangman/milestone_3.py
@@ -0,0 +1,34 @@
+import random
+
+word_list = ['banana', 'mango', 'lychee', 'strawberry', 'grapes']
+
+print(word_list)
+
+word = random.choice(word_list)
+
+print(word)
+
+
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print (f"Good guess! {guess} is in the word.")
+        
+    else:
+        print (f"Sorry, {guess} is not in the word. Try again.")
+
+
+def ask_for_input():
+    while True:
+        guess = input("Enter a single letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print ("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
+
+
+
+ask_for_input()
+
+
